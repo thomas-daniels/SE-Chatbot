23c0457f7764af6683965fc6c20c15c6345b4143
ProgramFOX <programfox@hotmail.be>
2014-09-21 11:23:00 +0200

    Don't only use nouns as associated words

--- GetAssociatedWord.py
+++ GetAssociatedWord.py
@@ -1,25 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 import random
+import re
 
 def GetAssociatedWord(w, latest_words):
     r = requests.get("http://wordassociations.net/search?q=%s" % w)
     soup = BeautifulSoup(r.text)
-    _list = soup.find_all("ul")
-    if _list is None or len(_list) < 3:
+    found_words = []
+    sections = soup.find_all("div", class_ = re.compile("(NOUN|ADJECTIVE|VERB|ADVERB)-SECTION"))
+    for section in sections:
+        list_items = section.find_all("li")
+        for li in list_items:
+            found_words.append(li.getText().lower())
+    if len(found_words) < 1:
         return (None, False)
     else:
         i = 0
-        word_list_contents = _list[2].contents
         already_tried = []
         x = 0
-        while x < len(word_list_contents):
-            i = random.randint(0, len(word_list_contents) - 1)
+        while x < len(found_words):
+            i = random.randint(0, len(found_words) - 1)
             if i in already_tried:
                 x -= 1
                 continue
             already_tried.append(i)
-            associated_word = _list[2].contents[i].contents[0].contents[0].lower()
+            associated_word = found_words[i]
             if not associated_word in latest_words:
                 return (associated_word, True)
             x += 1
