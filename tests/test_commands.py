5e1febdb3bd8cc1a5281b29969281c8b87284294
ProgramFOX <programfox@hotmail.be>
2015-07-01 12:41:26 +0200

    Fix unit tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,13 +1,17 @@
+from builtins.utils import *
 import Commands
 import pytest
 
 
-@pytest.mark.parametrize("command, args, expected_result", [
-    (Commands.command_alive, [], "Yes, I'm alive."),
-    (Commands.command_xkcd, ["1000"], "http://xkcd.com/1000/"),
+@pytest.mark.parametrize("command, args, expected_result, arg_count", [
+    (command_alive, [], "Yes, I'm alive.", 5),
+    (Commands.command_xkcd, ["1000"], "http://xkcd.com/1000/", 3),
     (Commands.command_xkcdrandomnumber, [],
-     "[4 // Chosen by fair dice roll. Guaranteed to be random.](http://xkcd.com/221/)"),
-    (Commands.command_xkcd, [], "Not enough arguments.")
+     "[4 // Chosen by fair dice roll. Guaranteed to be random.](http://xkcd.com/221/)", 3),
+    (Commands.command_xkcd, [], "Not enough arguments.", 3)
 ])
-def test_commands(command, args, expected_result):
-    assert command(args, None, None) == expected_result
+def test_commands(command, args, expected_result, arg_count):
+    if arg_count == 5:
+        assert command(None, None, args, None, None) == expected_result
+    else:
+        assert command(args, None, None) == expected_result
