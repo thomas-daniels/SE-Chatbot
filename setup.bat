7f0b12d13b748518b3589f2e9f2b75b023b50096
ProgramFOX <programfox@hotmail.be>
2015-07-01 16:36:02 +0200

    Add setup.bat

---
+++ setup.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/ProgramFOX/SE-Chatbot.git
+cd SE-Chatbot
+git submodule update --init
+pip install requests --upgrade
+pip install beautifulsoup4
+pip install websocket-client --upgrade
