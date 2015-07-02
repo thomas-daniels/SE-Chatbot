fe30cdab68b4bca5a3c8f99b54d600b36c1b7480
ProgramFOX <programfox@hotmail.be>
2015-07-02 12:03:43 +0200

    Tabs -> spaces

--- SaveIO.py
+++ SaveIO.py
@@ -26,15 +26,15 @@ def load(module, name):
     if module_dir not in _allowed_subdirs:
         raise InvalidDirectoryException("The subdirectory given is not a module's allowed subdirectory.")
     file = os.path.abspath(os.path.join(module_dir, name + ".p"))
-	if not os.path.exists(file) or os.stat(file).st_size == 0:
-		_create_empty_pickle_file(file)
+    if not os.path.exists(file) or os.stat(file).st_size == 0:
+        _create_empty_pickle_file(file)
     with open(file, "r") as f:
         return pickle.load(f)
         
 def _set_subdirs(dir_list):
     _allowed_subdirs = []
     for i in range len(dir_list):
-		dir = os.path.abspath(os.path.join(root_dir, dir_list[i]))
+        dir = os.path.abspath(os.path.join(root_dir, dir_list[i]))
         if dir in _allowed_subdirs:
             raise DuplicateDirectoryException("Duplicate name '" + dir + "'")
         _create_if_not_exists(dir)
@@ -46,10 +46,10 @@ def _create_if_not_exists(dir_path):
             os.makedirs(dir_path)
         except:
             print "Could not create directory %s." % (dir_path)
-			
+            
 def _create_empty_pickle_file(filepath):
-	with open(filepath, "w+") as f:
-		f.write("(dp0\n.")
+    with open(filepath, "w+") as f:
+        f.write("(dp0\n.")
             
 class InvalidDirectoryException(Exception):
     pass
