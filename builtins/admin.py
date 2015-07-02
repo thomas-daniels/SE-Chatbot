2e624e093d0e87876360199b07bcae65a930f7e7
overactor <rik.degraaff@gmx.ch>
2015-07-02 09:52:15 +0200

    finished command-specific ban and unban, fixes #20

--- builtins/admin.py
+++ builtins/admin.py
@@ -36,14 +36,23 @@ def command_ban(cmd, bot, args, msg, event):
         user_name = bot.client.get_user(banned_user).name.replace(" ", "")
     except:
         return "Could not fetch user; please check whether the user exists."
+    if len(args) > 1:
+        command = args[1]
+        if command not in command_banned_users:
+            command_banned_users[command] = [ ]
+        if banned_user not in command_banned_users[command]:
+            command_banned_users[command].append(banned_user)
+            saved_banned_users(bot)
+            return "User @%s has been banned from using >>%s." % (user_name, command)
+        else:
+            return "Already banned."
     if bot.site not in bot.banned:
         bot.banned[bot.site] = []
     if banned_user not in bot.banned[bot.site]:
         bot.banned[bot.site].append(banned_user)
     else:
         return "Already banned."
-    with open("bannedUsers.txt", "w") as f:
-        pickle.dump(bot.banned, f)
+    saved_banned_users(bot)
     return "User @%s has been banned." % user_name
 
 
@@ -56,14 +65,24 @@ def command_unban(cmd, bot, args, msg, event):
         user_name = bot.client.get_user(banned_user).name.replace(" ", "")
     except:
         return "Could not fetch user; please check whether the user exists."
-    if bot.site not in bot.banned:
-        return "Not banned."
-    if banned_user not in bot.banned[bot.site]:
-        return "Not banned."
-    bot.banned[bot.site].remove(banned_user)
-    with open("bannedUsers.txt", "w") as f:
-        pickle.dump(bot.banned, f)
-    return "User @%s has been unbanned." % user_name
+    if len(args) > 1:
+        command = args[1]
+        if command in command_banned_users and banned_user in command_banned_users[command]:
+            command_banned_users[command].remove(banned_user)
+            if len(command_banned_users[command])==0:
+                del command_banned_users[command]
+            saved_banned_users(bot)
+            return "User @%s has been unbanned from using >>%s." % (user_name, command)
+        else:
+            return "Not banned"
+    else:
+        if bot.site not in bot.banned:
+            return "Not banned."
+        if banned_user not in bot.banned[bot.site]:
+            return "Not banned."
+        bot.banned[bot.site].remove(banned_user)
+        saved_banned_users(bot)
+        return "User @%s has been unbanned." % user_name
 
 
 def command_delete(cmd, bot, args, msg, event):
@@ -83,22 +102,33 @@ commands = [
     Command('stop', command_stop, "Owner-only command. Stops the bot. Syntax: `>>stop`", False, True),
     Command('disable', command_disable, "Owner-only command. Disables the bot. Syntax: `>>disable`", False, True),
     Command('enable', command_enable, "Owner-only command. Enables the bot when it is disabled. Syntax: `>>enable`", False, True),
-    Command('ban', command_ban, "Owner-only command. Bans a user from using the bot. Syntax: `>>ban user_id`", False, True),
-    Command('unban', command_unban, "Owner-only command. Unbans a banned user. Syntax: `>>unban user_id`", False, True),
+    Command('ban', command_ban, "Owner-only command. Bans a user from using the bot. Syntax: `>>ban user_id [command]`", False, True),
+    Command('unban', command_unban, "Owner-only command. Unbans a banned user. Syntax: `>>unban user_id` [command]", False, True),
     Command('delete', command_delete, "Only for privileged users. Deletes a message of the bot. Syntax: `>>delete msg_id` or `<reply> !delete!`", True, True)
 ]
 
 command_banned_users = { }
 
-def test_deco(func):
-    def print_deco(cmd, msg, event, *args, **kwargs):
+def saved_banned_users(bot):
+    global command_banned_users
+    with open("bannedUsers.txt", "w") as f:
+            pickle.dump({ "all_banned": bot.banned, "command_banned": command_banned_users}, f)
+
+def ban_deco(func):
+    def check_banned(cmd, msg, event, *args, **kwargs):
         cmd_args = cmd.split(' ')
         cmd_name = cmd_args[0].lower()
         if cmd_name not in command_banned_users or event.user.id not in command_banned_users[cmd_name]:
             return func(cmd, msg, event, *args, **kwargs)
         else:
             return "You have been banned from using that command."
-    return print_deco
+    return check_banned
 
 def on_bot_load(bot):
-    bot.command = test_deco(bot.command)
+    global command_banned_users
+    bot.command = ban_deco(bot.command)
+    if os.path.isfile("bannedUsers.txt"):
+        with open("bannedUsers.txt", "r") as f:
+            dump = pickle.load(f)
+            if 'command_banned' in dump:
+                command_banned_users = dump['command_banned']
