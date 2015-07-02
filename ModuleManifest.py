9408b8c88e8c8bdb719791e5b77b622bbe2c54b9
ProgramFOX <programfox@hotmail.be>
2015-07-02 15:55:56 +0200

    Add some comments to ModuleManifest.py

--- ModuleManifest.py
+++ ModuleManifest.py
@@ -1,3 +1,9 @@
 module_file_names = [
     'builtins.meta'
 ]
+
+# You can add more modules there in the following format:
+# 1. Drop the .py from a file name.
+# 2. In case your module is in a subdirectory,
+#    make sure that there is an __init__.py file in that subdirectory,
+#    and replace (back)slashes with dots when you add them here.
