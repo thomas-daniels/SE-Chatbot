e080def22f598b864228077444f2ac79585f7712
overactor <rik.degraaff@gmx.ch>
2015-07-08 11:44:34 +0200

    Call listeners regardless of event type.

--- Chatbot.py
+++ Chatbot.py
@@ -187,14 +187,16 @@ class Chatbot:
         return False
 
     def on_event(self, event, client):
-        if not (isinstance(event, MessagePosted) or isinstance(event, MessageEdited)) \
-            or (not self.enabled and event.user.id not in self_owner_ids) \
+        if  (not self.enabled and event.user.id not in self_owner_ids) \
             or not self.running:
             return
         
         watchers = self.modules.get_event_watchers()
         for w in watchers:
             w(event, client, self)
+        
+        if not (isinstance(event, MessagePosted) or isinstance(event, MessageEdited)):
+            return
 
         if event.user.id == self.client.get_me().id:
             return
