cec1ddb29b69b0ba0e221e9d3cbc987b4133ff5e
ProgramFOX <programfox@hotmail.be>
2015-07-22 13:05:15 +0200

    Update setup.sh for Python 3

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 git submodule init
 git submodule update
-sudo pip install beautifulsoup4
-sudo pip install requests --upgrade
-sudo pip install websocket-client --upgrade
-sudo pip install requests[security] --upgrade
+sudo apt-get update
+sudo apt-get install python3-setuptools
+sudo easy_install3 pip
+sudo pip3 install beautifulsoup4
+sudo pip3 install requests --upgrade
+sudo pip3 install websocket-client --upgrade
+sudo pip3 install requests[security] --upgrade
 echo Now copy templates/ConfigTemplate.py to ./Config.py and edit the important values there.
 echo Then, run the bot using python main.py, where python is Python 2.
 echo Press any key to close the setup...
