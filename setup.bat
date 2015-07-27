4ba50335faf53d2881c753bbce3506f7f700a6fc
ProgramFOX <programfox@hotmail.be>
2015-07-27 12:42:07 +0200

    Update info in setup files to Py3

--- setup.bat
+++ setup.bat
@@ -5,5 +5,5 @@ pip install beautifulsoup4
 pip install websocket-client --upgrade
 pip install requests[security] --upgrade
 echo Now copy templates/ConfigTemplate.py to ./Config.py and edit the important values there.
-echo Then, run the bot using python main.py, where python is Python 2.
+echo Then, run the bot using python3 main.py.
 pause
--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ sudo pip3 install requests --upgrade
 sudo pip3 install websocket-client --upgrade
 sudo pip3 install requests[security] --upgrade
 echo Now copy templates/ConfigTemplate.py to ./Config.py and edit the important values there.
-echo Then, run the bot using python main.py, where python is Python 2.
+echo Then, run the bot using python3 main.py.
 echo Press any key to close the setup...
 read -n 1 -s
