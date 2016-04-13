7ae0d9726343c47f838c6337c9ec9fa0f8a433b4
ProgramFOX <programfox@hotmail.be>
2016-04-13 17:40:59 +0200

    Add pull command

--- run.sh
+++ run.sh
@@ -10,13 +10,21 @@
 
 stoprunning=0
 firststart=1
+ec=0
 while [ "$stoprunning" -eq "0" ]
 do
     if [ "$firststart" -eq "1" ]
     then
         python3 main.py "$@"
-    else
+    elif [ "$firststart" -eq "0" ] && [ "$ec" -eq "1" ]
+    then
         python3 main.py "$@" -m "Bot restarted after crash."
+    elif [ "$firststart" -eq "0" ] && [ "$ec" -eq "2" ]
+    then
+        rev=git rev-parse HEAD
+        python3 main.py "$@" -m "Bot restarted after pulling from the GitHub repository. Running on revision `$rev`."
+    else
+        python3 main.py "$@" -m "Bot restarted after terminating with an unrecognized status code."
     fi
 
     ec=$?
@@ -24,6 +32,12 @@ do
     if [ "$ec" -eq "0" ]
     then
         stoprunning=1
+    elif [ "$ec" -eq "2" ]
+    then
+        git fetch origin master
+        git merge origin/master --no-edit
+        git submodule update --init --recursive
+        firststart = 0
     else
         firststart=0
     fi
